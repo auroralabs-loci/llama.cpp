463f536695e85dc54b4a45d5c0ee87f3800eaecc
liyang <liyang2@uniontech.com>
2025-10-31 16:28:58 +0800

    address #16574; fold CLI into mtmd-cli; use ggml_rope_ext + bicubic;switch to 'jinaclip2'; fix converter constants

--- gguf-py/gguf/constants.py
+++ gguf-py/gguf/constants.py
@@ -3604,6 +3604,7 @@ class VisionProjectorType:
     QWEN3VL = "qwen3vl_merger"
     ULTRAVOX = "ultravox"
     INTERNVL = "internvl"
+    JINACLIP2 = "jinaclip2"
     QWEN2A = "qwen2a" # audio
     GLMA = "glma" # audio
     QWEN25O = "qwen2.5o" # omni
