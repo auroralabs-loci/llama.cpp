fc59b6ddc2e897b97a331e2dc88d4b8d3a307fa5
Piotr Wilkin <piotr.wilkin@syndatis.com>
2025-12-13 15:56:54 +0100

    THE GIANT AUTOPARSER SQUISH

--- tests/test-jinja.cpp
+++ tests/test-jinja.cpp
@@ -401,6 +401,13 @@ static void test_set_statement(testing & t) {
         json::object(),
         "1"
     );
+
+    test_template(t, "set thinking",
+        "{%- if not thinking is defined -%}{%- if enable_thinking is defined -%}{%- set thinking = enable_thinking -%}{%- else -%}{%- set thinking = false %}{%- endif -%}{%- endif -%}{{ thinking }}",
+        {{"enable_thinking",true}},
+        "True"
+    );
+
 }
 
 static void test_filters(testing & t) {
@@ -962,6 +969,17 @@ static void test_tests(testing & t) {
         "yes"
     );
 
+    test_template(t, "select in",
+        "{% for item in items|select('in', '0020') %}{{ item }} {% endfor %}",
+        {{"items", json::array({"1", "0", "20"})}},
+        "0 20 "
+    );
+
+    test_template(t, "select in",
+        "{% for item in items|select('in', ['0','20']) %}{{ item }} {% endfor %}",
+        {{"items", json::array({0, "0", false, "20"})}},
+        "0 20 "
+    );
 }
 
 static void test_string_methods(testing & t) {
