4a14540264f2fd7a17ceb4ed64d625288e5efcdd
Daniel Bevenius <daniel.bevenius@gmail.com>
2026-01-07 10:25:58 +0100

    model-conversion : add detect_pooling script
    This commit adds a Python script to automatically detect the pooling
    configuration from a sentence-transformers model directory.
    
    The motivation for this change is that I make a mistake when adding the
    sentence-transformers support and I incorrectly assumed that if an
    embedding model uses sentence-transformers, it always used pooling. With
    the recent addition of support for late interaction models, which can
    have a down-projection but do not use pooling (like LFM2-ColBert-350M).
    
    This commit builds upon https://github.com/ggml-org/llama.cpp/pull/18464
    which needs to be merged first.
    
    Refs: https://github.com/ggml-org/llama.cpp/pull/18607#issuecomment-3710530332

--- examples/model-conversion/scripts/embedding/run-converted-model.sh
+++ examples/model-conversion/scripts/embedding/run-converted-model.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 # Parse command line arguments
 CONVERTED_MODEL=""
 PROMPTS_FILE=""
-USE_POOLING=""
+POOLING=""
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -14,8 +14,8 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --pooling)
-            USE_POOLING="1"
-            shift
+            POOLING="$2"
+            shift 2
             ;;
         *)
             if [ -z "$CONVERTED_MODEL" ]; then
@@ -50,10 +50,5 @@ fi
 
 echo $CONVERTED_MODEL
 
-cmake --build ../../build --target llama-logits -j8
-# TODO: update logits.cpp to accept a --file/-f option for the prompt
-if [ -n "$USE_POOLING" ]; then
-    ../../build/bin/llama-logits -m "$CONVERTED_MODEL" -embd-mode -pooling "$PROMPT"
-else
-    ../../build/bin/llama-logits -m "$CONVERTED_MODEL" -embd-mode "$PROMPT"
-fi
+cmake --build ../../build --target llama-debug -j8
+../../build/bin/llama-debug -m "$CONVERTED_MODEL" --embedding $POOLING -p "$PROMPT" --save-logits
