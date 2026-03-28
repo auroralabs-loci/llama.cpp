2761dcaf92023dc7debb01a828af810b53f80c88
Michael Wand <michael@baybridgeaquarium.com>
2026-03-27 19:11:45 -0700

    Added extra FP8 guard, hope to solve ci HIP failure

--- ggml/src/ggml-cuda/mmq.cu
+++ ggml/src/ggml-cuda/mmq.cu
@@ -372,3 +372,7 @@ bool ggml_cuda_should_use_mmq(enum ggml_type type, int cc, int64_t ne11, int64_t
     return (!GGML_CUDA_CC_IS_CDNA(cc)) || ne11 < MMQ_DP4A_MAX_BATCH_SIZE;
 
 }
+
+#ifdef FP8_AVAILABLE
+DECL_MMQ_CASE(GGML_TYPE_NVFP4);
+#endif // FP8_AVAILABLE
